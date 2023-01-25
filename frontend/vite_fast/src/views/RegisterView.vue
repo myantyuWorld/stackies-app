8743b37828a31f6c8abaf582b361d61e83875c34
myantyuWorld <nepinepi984@gmail.com>
2023-01-25 19:09:20 +0900

    Modify 基本情報登録画面 Modal

--- frontend/vite_fast/src/views/RegisterView.vue
+++ frontend/vite_fast/src/views/RegisterView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import MasterData from '../components/MasterData.vue'
 import MasterDataItem from '../components/MasterDataItem.vue'
+import Rating from '../components/Rating.vue'
+import ExperienceRating from '../components/ExperienceRating.vue'
 
 import { onMounted } from 'vue'
 import { initModals } from 'flowbite'
@@ -153,18 +155,12 @@ onMounted(() => {
                       </div>
                       <!-- Modal body -->
                       <div class="p-6 space-y-6">
-                        <div class="justify-between p-4 rounded-lg bg-white shadow-indigo-50 shadow-md my-2">
-
-                          <div class="flex flex-wrap -m-4 text-center">
-                            <div class="p-1 sm:w-1/2 lg:w-1/4 w-full hover:scale-105 duration-500">
-                              <MasterDataItem :badgeColor=itemBadgeColor />
-                            </div>
-                            <div class="p-1 sm:w-1/2 lg:w-1/4 w-full hover:scale-105 duration-500">
-                              
-                            </div>
-                          </div>
-
-                        </div>
+                        <ExperienceRating/>
+                        <ExperienceRating/>
+                        <ExperienceRating/>
+                        <ExperienceRating/>
+                        <ExperienceRating/>
+                        <ExperienceRating/>
                       </div>
                       <!-- Modal footer -->
                       <div
@@ -198,36 +194,10 @@ onMounted(() => {
                         経験
                       </label>
 
-                      <div class="grid w-full grid-cols-4 space-x-2 rounded-xl bg-gray-200 p-2" x-data="app">
-                        <div>
-                          <input type="radio" name="option" id="1" class="peer hidden" checked />
-                          <label for="1"
-                            class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">1</label>
-                        </div>
-
-                        <div>
-                          <input type="radio" name="option" id="2" class="peer hidden" />
-                          <label for="2"
-                            class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">2</label>
-                        </div>
-
-                        <div>
-                          <input type="radio" name="option" id="3" class="peer hidden" />
-                          <label for="3"
-                            class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">3</label>
-                        </div>
-
-                        <div>
-                          <input type="radio" name="option" id="4" class="peer hidden" />
-                          <label for="4"
-                            class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">4</label>
-                        </div>
-                      </div>
-
+                      <ExperienceRating/>
+                      <ExperienceRating/>
+                      <ExperienceRating/>
                     </div>
-
-
-
 
                     <div class="mt-10">
                       <input type="submit" value="基本情報登録"
