aa701cef379a966f80dc9147327576d44d458365
myantyuWorld <nepinepi984@gmail.com>
2023-01-26 00:07:23 +0900

    Modify 案件対応履歴

--- frontend/vite_fast/src/views/ProjectListView.vue
+++ frontend/vite_fast/src/views/ProjectListView.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import ExperienceRating from '../components/ExperienceRating.vue'
-
 import { onMounted } from 'vue'
 import { initDropdowns, } from 'flowbite'
 
@@ -20,7 +18,7 @@ onMounted(() => {
             <div class="p-6 sm:p-6">
 
               <div class="space-y-4">
-                <h2 class="mb-0 text-2xl text-cyan-900 font-bold">基本情報登録</h2>
+                <h2 class="mb-0 text-2xl text-cyan-900 font-bold">案件対応履歴</h2>
               </div>
 
               <div class="flex bg-white">
@@ -116,7 +114,7 @@ onMounted(() => {
 
               <div class="space-y-0">
                 <h2 class="mb-0 text-2xl text-cyan-900 font-bold">
-                  経験技術
+                  業務履歴
                 </h2>
               </div>
 
@@ -136,11 +134,11 @@ onMounted(() => {
                     <!-- https://flowbite.com/docs/components/tables/#table-caption -->
                     <div class="w-full mx-auto my-5">
                       <caption
-                      class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-                      Our products
-                      <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite
-                        products designed to help you work and play, stay organized, get answers, keep in touch, grow
-                        your business, and more.</p>
+                        class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                        Our products
+                        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite
+                          products designed to help you work and play, stay organized, get answers, keep in touch, grow
+                          your business, and more.</p>
                       </caption>
                     </div>
 
@@ -268,6 +266,59 @@ onMounted(() => {
 
                       </tbody>
                     </table>
+
+                    <nav class="flex items-center justify-between pt-4" aria-label="Table navigation">
+                      <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Showing <span
+                          class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span
+                          class="font-semibold text-gray-900 dark:text-white">1000</span></span>
+                      <ul class="inline-flex items-center -space-x-px">
+                        <li>
+                          <a href="#"
+                            class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span class="sr-only">Previous</span>
+                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd"
+                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                clip-rule="evenodd"></path>
+                            </svg>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#"
+                            class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                        </li>
+                        <li>
+                          <a href="#"
+                            class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                        </li>
+                        <li>
+                          <a href="#" aria-current="page"
+                            class="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                        </li>
+                        <li>
+                          <a href="#"
+                            class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
+                        </li>
+                        <li>
+                          <a href="#"
+                            class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
+                        </li>
+                        <li>
+                          <a href="#"
+                            class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span class="sr-only">Next</span>
+                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"></path>
+                            </svg>
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+
                   </div>
 
                 </div>
