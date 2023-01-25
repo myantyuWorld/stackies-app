acedc3dff5c7b3a02745eb1a2af2160602eb4efe
myantyuWorld <nepinepi984@gmail.com>
2023-01-25 18:54:20 +0900

    Modify 基本情報登録画面 Modal

--- frontend/vite_fast/src/views/RegisterView.vue
+++ frontend/vite_fast/src/views/RegisterView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import MasterData from '../components/MasterData.vue'
+import MasterDataItem from '../components/MasterDataItem.vue'
 
 import { onMounted } from 'vue'
 import { initModals } from 'flowbite'
@@ -132,11 +133,11 @@ onMounted(() => {
                   class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
                   <div class="relative w-full h-full max-w-2xl md:h-auto">
                     <!-- Modal content -->
-                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div class="relative bg-white rounded-lg shadow-xl dark:bg-gray-700">
                       <!-- Modal header -->
                       <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                          Terms of Service
+                          経験技術
                         </h3>
                         <button type="button"
                           class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -152,26 +153,26 @@ onMounted(() => {
                       </div>
                       <!-- Modal body -->
                       <div class="p-6 space-y-6">
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                          With less than a month to go before the European Union enacts new consumer privacy laws for
-                          its citizens, companies around the world are updating their terms of service agreements to
-                          comply.
-                        </p>
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                          The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
-                          and is meant to ensure a common set of data rights in the European Union. It requires
-                          organizations to notify users as soon as possible of high-risk data breaches that could
-                          personally affect them.
-                        </p>
+                        <div class="justify-between p-4 rounded-lg bg-white shadow-indigo-50 shadow-md my-2">
+
+                          <div class="flex flex-wrap -m-4 text-center">
+                            <div class="p-1 sm:w-1/2 lg:w-1/4 w-full hover:scale-105 duration-500">
+                              <MasterDataItem :badgeColor=itemBadgeColor />
+                            </div>
+                            <div class="p-1 sm:w-1/2 lg:w-1/4 w-full hover:scale-105 duration-500">
+                              
+                            </div>
+                          </div>
+
+                        </div>
                       </div>
                       <!-- Modal footer -->
                       <div
                         class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
                         <button data-modal-hide="defaultModal" type="button"
-                          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                          accept</button>
+                          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">追加</button>
                         <button data-modal-hide="defaultModal" type="button"
-                          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">閉じる</button>
                       </div>
                     </div>
                   </div>
@@ -196,6 +197,7 @@ onMounted(() => {
                         for="grid-password">
                         経験
                       </label>
+
                       <div class="grid w-full grid-cols-4 space-x-2 rounded-xl bg-gray-200 p-2" x-data="app">
                         <div>
                           <input type="radio" name="option" id="1" class="peer hidden" checked />
@@ -221,6 +223,7 @@ onMounted(() => {
                             class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">4</label>
                         </div>
                       </div>
+
                     </div>
 
 
