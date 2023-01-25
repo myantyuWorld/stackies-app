0eccf593a8944e76b87aca0aac8c0f17e2562a51
myantyuWorld <nepinepi984@gmail.com>
2023-01-25 15:48:30 +0900

    Modify MasterDataView.vue

---
+++ frontend/vite_fast/src/components/MasterData.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class=" flex items-center  justify-between p-4  rounded-lg bg-white shadow-indigo-50 shadow-md">
+
+      <div
+        class="bg-gradient-to-tr from-blue-500 to-blue-400 w-16 h-16  rounded-full shadow-2xl shadow-blue-400 border-white  border-dashed border-2  flex justify-center items-center ">
+        <div>
+          <h1 class="text-white text-xs">Java</h1>
+        </div>
+      </div>
+
+      <div>
+        <button
+          class="text-xs mt-0 px-3 py-3 bg-black-400 text-white rounded-lg  tracking-wider hover:bg-black-300 outline-none">Del</button>
+      </div>
+
+    </div>
+
+</template>
