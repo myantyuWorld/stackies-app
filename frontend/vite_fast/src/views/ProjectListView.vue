bd04de5949de6dfc3bb34908fe3578fe037c3e1f
myantyuWorld <nepinepi984@gmail.com>
2023-01-28 20:59:05 +0900

    Modify 案件対応履歴

--- frontend/vite_fast/src/views/ProjectListView.vue
+++ frontend/vite_fast/src/views/ProjectListView.vue
@@ -2,7 +2,8 @@
 import { onMounted } from 'vue'
 import { initDropdowns, } from 'flowbite'
 
-import  ProjectListTable from '../components/ProjectListTable.vue'
+import ProjectListTable from '../components/ProjectListTable.vue'
+import BaseInfo from '../components/BaseInfo.vue'
 
 // initialize components based on data attribute selectors
 onMounted(() => {
@@ -18,108 +19,13 @@ onMounted(() => {
         <div class="m-auto">
           <div class="rounded-xl bg-white shadow-xl">
             <div class="p-6 sm:p-6">
-
               <div class="space-y-4">
                 <h2 class="mb-0 text-2xl text-cyan-900 font-bold">案件対応履歴</h2>
               </div>
-
               <div class="flex bg-white">
-                <div class="w-full px-4 shadow-none flex flex-wrap ">
-                  <!-- イニシャル、住所、資格（名前、取得時期）、学歴、生年月日、自己PR） -->
-                    <div class="p-1 w-1/12">
-                      <div>
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-password">
-                          イニシャル
-                        </label>
-                        <input type="text"
-                          class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                          placeholder="" disabled value="Y O">
-                      </div>
-
-                    </div>
-                    <div class="p-1 w-2/12">
-
-                      <div>
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-password">
-                          生年月日
-                        </label>
-                        <input type="date"
-                          class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                          placeholder="生年月日" disabled>
-                      </div>
-
-                    </div>
-                    <div class="p-1 w-4/12">
-
-                      <div>
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-password">
-                          最終学歴
-                        </label>
-                        <input type="text"
-                          class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                          placeholder="最終学歴" disabled>
-                      </div>
-
-                    </div>
-                    <div class="p-1 w-5/12">
-                    <div>
-                      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                        for="grid-password">
-                        資格
-                      </label>
-                      <input type="text"
-                        class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                        placeholder="資格" disabled>
-                    </div>
-
-                    </div>
-                    <div class="p-1 w-3/12">
-                      <div>
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-password">
-                          郵便番号
-                        </label>
-                        <input type="text"
-                          class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                          placeholder="住所" disabled>
-                      </div>
-
-                    </div>
-                    <div class="p-1 w-9/12">
-
-                      <div>
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-password">
-                          住所
-                        </label>
-                        <input type="text"
-                          class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                          placeholder="住所" disabled>
-                      </div>
-
-                    </div>
-
-
-                    <div class="p-1 w-full">
-
-                      <div>
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                          for="grid-password">
-                          自己PR
-                        </label>
-                        <textarea type="text"
-                          class="block w-full p-2 border rounded border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-transparent disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-                          placeholder="Email" disabled></textarea>
-                      </div>
-
-                    </div>
-
-                </div>
+                <!-- TODO : 入力モード、表示モードをpropで切り替えられるようにする -->
+                <BaseInfo/>
               </div>
-
             </div>
             <div class="p-6 sm:p-6">
 
@@ -128,91 +34,11 @@ onMounted(() => {
                   業務履歴
                 </h2>
               </div>
-
               <div class="flex bg-white">
                 <div class="w-full px-4 shadow-none">
-                  <div class="m-4">
-                    <!-- https://flowbite.com/docs/components/tables/#table-caption -->
-                    <div class="w-full mx-auto my-5">
-                      <caption
-                        class="text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-                        Our products
-                        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite
-                          products designed to help you work and play, stay organized, get answers, keep in touch, grow
-                          your business, and more.</p>
-                      </caption>
-                    </div>
-
-                    <!-- https://flowbite.com/docs/components/tables/#table-filter -->
-                    <ProjectListTable/>
-
-                    <nav class="flex flex-wrap text-center items-center justify-between pt-4"
-                      aria-label="Table navigation">
-
-                      <div class="sm:w-1/2 mb-4">
-                        <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Showing <span
-                            class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span
-                            class="font-semibold text-gray-900 dark:text-white">1000</span></span>
-                      </div>
-
-                      <div class="sm:w-1/2">
-
-                        <ul class="inline-flex items-center -space-x-px">
-                          <li>
-                            <a href="#"
-                              class="block px-2 py-1 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                              <span class="sr-only">Previous</span>
-                              <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                  clip-rule="evenodd"></path>
-                              </svg>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#"
-                              class="px-2 py-1 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                          </li>
-                          <li>
-                            <a href="#"
-                              class="px-2 py-1 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                          </li>
-                          <li>
-                            <a href="#" aria-current="page"
-                              class="z-10 px-2 py-1 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                          </li>
-                          <li>
-                            <a href="#"
-                              class="px-2 py-1 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
-                          </li>
-                          <li>
-                            <a href="#"
-                              class="px-2 py-1 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
-                          </li>
-                          <li>
-                            <a href="#"
-                              class="block px-2 py-1 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                              <span class="sr-only">Next</span>
-                              <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                  clip-rule="evenodd"></path>
-                              </svg>
-                            </a>
-                          </li>
-                        </ul>
-
-                      </div>
-
-                    </nav>
-
-                  </div>
-
+                  <ProjectListTable/>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
