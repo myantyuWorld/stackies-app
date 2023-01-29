cf49480f318b2f53dca7e68e0f035fc4389b6c08
myantyuWorld <nepinepi984@gmail.com>
2023-01-29 22:03:47 +0900

    Add Todo comment

--- frontend/vite_fast/src/components/MasterData.vue
+++ frontend/vite_fast/src/components/MasterData.vue
@@ -11,8 +11,9 @@ defineProps<{
   <div class="rounded-xl bg-white shadow-xl p-6 my-6">
     <div class="container mt-8 px-5 py-1 mx-auto my-auto">
       <h2 class="mb-8 text-xl text-cyan-900 font-bold">{{ name }}
-        <button
-          class="text-sm ml-6 px-4 py-2 bg-gray-400  text-white rounded-lg  tracking-wider hover:bg-gray-500 outline-none">Add</button>
+        <!-- TODO：本画面で、言語など追加できるようにする -->
+        <!-- <button
+          class="text-sm ml-6 px-4 py-2 bg-gray-400  text-white rounded-lg  tracking-wider hover:bg-gray-500 outline-none">Add</button> -->
       </h2>
 
     </div>
