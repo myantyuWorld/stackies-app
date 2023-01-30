7e9f421febb715a88791e2c0bd703dacfc4ca1dc
myantyuWorld <nepinepi984@gmail.com>
2023-01-30 18:49:15 +0900

    Ratingコンポーネント、星マークに変更

--- frontend/vite_fast/src/components/ExperienceRating.vue
+++ frontend/vite_fast/src/components/ExperienceRating.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 defineProps<{
-  itemName: string
+  itemName: string,
+  experienceRate : int
 }>()
 import MasterDataItem from '../components/MasterDataItem.vue'
 import Rating from '../components/Rating.vue'
@@ -8,10 +9,10 @@ import Rating from '../components/Rating.vue'
 <template>
 
   <div class="justify-between p-2 rounded-lg bg-white shadow-indigo-50 shadow-md my-2">
-    <div class="flex flex-wrap -m-4 text-center">
+    <div class="flex flex-wrap -m-4 p-4 justify-center">
       <MasterDataItem :name=itemName />
       <div class="p-1 sm:w-2/3 lg:w-3/4 w-full hover:scale-105 duration-500">
-        <Rating />
+        <Rating :rate=experienceRate />
       </div>
     </div>
 
--- frontend/vite_fast/src/components/Rating.vue
+++ frontend/vite_fast/src/components/Rating.vue
@@ -1,33 +1,11 @@
 <script setup lang="ts">
-// defineProps<{
-//   badgeColor: string
-// }>()
+defineProps<{
+  rate: int
+}>()
 </script>
 <template>
-  <!-- <div class="grid w-full grid-cols-4 space-x-2 rounded-xl bg-white my-auto p-2" x-data="app"> -->
-  <div class=" flex items-center  justify-between p-4  rounded-lg bg-white shadow-indigo-50 shadow-md" x-data="app">
-    <div>
-      <input type="radio" name="option" id="1" class="peer hidden" checked />
-      <label for="1"
-        class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">1</label>
-    </div>
-
-    <div>
-      <input type="radio" name="option" id="2" class="peer hidden" />
-      <label for="2"
-        class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">2</label>
-    </div>
-
-    <div>
-      <input type="radio" name="option" id="3" class="peer hidden" />
-      <label for="3"
-        class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">3</label>
-    </div>
-
-    <div>
-      <input type="radio" name="option" id="4" class="peer hidden" />
-      <label for="4"
-        class="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">4</label>
-    </div>
+  <div class="flex items-center">
+    <svg aria-hidden="true" class="w-10 h-10 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" v-for="n of rate" :key="n" ><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+    <svg aria-hidden="true" class="w-10 h-10 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" v-for="n of 5 - rate" :key="n"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
   </div>
 </template>
