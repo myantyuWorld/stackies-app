281c08ecbe6da59c71262b1ebf4c01067e712e0a
myantyuWorld <nepinepi984@gmail.com>
2023-01-31 19:09:10 +0900

    Ratingを操作に応じた動的な値で表示

--- frontend/vite_fast/src/components/ExperienceRating.vue
+++ frontend/vite_fast/src/components/ExperienceRating.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
+import { expressionStatement } from '@babel/types';
 import { ref } from 'vue';
 import MasterDataItem from '../components/MasterDataItem.vue'
 import Rating from '../components/Rating.vue'
 
-defineProps<{
+const data = defineProps<{
   itemName: string,
   experienceRate: int
 }>()
 
 const selected = ref(0)
-console.log(selected.value)
+selected.value = data.experienceRate
+
 // selected = data.experienceRate
 
 </script>
@@ -22,7 +24,7 @@ console.log(selected.value)
       <div class="p-1 w-1/4 hover:scale-105 duration-500">
         <select v-model="selected" id="countries"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option :value="n" v-for="n of 5" :key="n" :selected="n == experienceRate">{{ n }}</option>
+          <option :value="n" v-for="n of 5" :key="n">{{ n }}</option>
         </select>
       </div>
       <div class="p-1 sm:w-1/4 lg:w-2/4 w-full hover:scale-105 duration-500">
