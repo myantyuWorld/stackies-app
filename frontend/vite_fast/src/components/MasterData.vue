7c15b151be7c6594811570a51b0cee6c9335419e
myantyuWorld <nepinepi984@gmail.com>
2023-02-02 17:06:57 +0900

    MasterDataItem, チェックした値をv-model(配列）で格納

--- frontend/vite_fast/src/components/MasterData.vue
+++ frontend/vite_fast/src/components/MasterData.vue
@@ -8,7 +8,7 @@ const data = defineProps<{
   list: object
 }>()
 
-const checkItem = (checked : string) => {
+const checkItem = (checked: string) => {
   console.log(checked)
 }
 
@@ -28,7 +28,15 @@ const checkedValues = ref([])
       {{ checkedValues }}
     </div>
     <div class="flex flex-wrap w-full">
-      <MasterDataItem :value="item.value" :name=item.name v-for="item of itemList" :key="item.name" @checkValue="checkItem"/>
+      <!-- // TODO: MasterDataItemのチェック状態を、"checkedValues"に格納できない -->
+      <!-- <MasterDataItem :id="item.name" :value="item.value" :name=item.name v-for="item of itemList" :key="item.name" @checkValue="checkItem" v-model="checkedValues"/> -->
+
+      <!-- この書き方なら、"checkedValues"に値を配列で格納できる -->
+      <div class="w-1/4 items-center mb-4" v-for="item of itemList" :key="item.name">
+        <input :id="item.name" type="checkbox" :value="item.value" @input="$emit('checkValue', $event.target.value)" v-model="checkedValues"
+          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label :for="item.name" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ item.name }}</label>
+      </div>
     </div>
   </div>
 
