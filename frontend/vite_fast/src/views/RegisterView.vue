43e9c6bccf1a4d41e903f4aff7754981f69f2d11
myantyuWorld <nepinepi984@gmail.com>
2023-02-02 22:07:20 +0900

    Modify add

--- frontend/vite_fast/src/views/RegisterView.vue
+++ frontend/vite_fast/src/views/RegisterView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import router from '@/router';
+import { ref } from 'vue';
 import { onMounted } from 'vue'
 import { initModals } from 'flowbite'
 import ExperienceRating from '../components/ExperienceRating.vue'
 import Rating from '../components/Rating.vue'
 import BaseInfo from '../components/BaseInfo.vue'
-
 
 // initialize components based on data attribute selectors
 onMounted(() => {
@@ -16,15 +16,36 @@ const click_regist = () => {
   router.push('menu')
 }
 
-const baseinfo = {
-  initial: "あああ",
-  birth_date: "1990-08-20",
-  last_educational_background: "ほげほげ専門学校",
-  qualification: "応用情報技術者, AWS CLF",
-  postcode: "7310102",
-  address: "広島県広島市hogehogeのほげ",
-  self_pr: "あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ"
-}
+const data = ref({
+  baseinfo: {
+    initial: "あああ",
+    birth_date: "1990-08-20",
+    last_educational_background: "ほげほげ専門学校",
+    qualification: "応用情報技術者, AWS CLF",
+    postcode: "7310102",
+    address: "広島県広島市hogehogeのほげ",
+    self_pr: "あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ"
+  },
+  experienceRateInfo: [
+    {
+      id: "1",
+      name: "Java",
+      level: "3",
+    },
+    {
+      id: "2",
+      name: "C#",
+      level: "3",
+    },
+    {
+      id: "3",
+      name: "javascript",
+      level: "3",
+    }
+  ]
+})
+const baseinfo = data.value.baseinfo
+const experienceRate = data.value.experienceRateInfo
 
 </script>
 
@@ -38,6 +59,7 @@ const baseinfo = {
 
               <div class="space-y-4">
                 <h2 class="mb-0 text-2xl text-cyan-900 font-bold">案件対応履歴</h2>
+                {{ data }}
               </div>
               <div class="flex bg-white">
                 <BaseInfo :inputMode="false" :initial="baseinfo.initial" :birth_date="baseinfo.birth_date"
@@ -71,27 +93,15 @@ const baseinfo = {
                       </svg>
                     </button>
                   </h2>
-                  <div id="accordion-flush-body-1" class="hidden bg-gray-50 " aria-labelledby="accordion-flush-heading-1">
+                  <div id="accordion-flush-body-1" class="hidden bg-gray-50 "
+                    aria-labelledby="accordion-flush-heading-1">
                     <div class="flex flex-wrap justify-items-center">
+
                       <div class="w-1/4 p-1">
-                        <Rating rate="5" />
+                        <Rating rate="1" />
                       </div>
                       <div class="w-1/4 p-1">
-                        指導可能
-                      </div>
-                      <div class="w-1/4 p-1">
-                        <Rating rate="4" />
-                      </div>
-                      <div class="w-1/4 p-1">
-                        技術精通
-                      </div>
-                    </div>
-                    <div class="flex flex-wrap justify-items-center">
-                      <div class="w-1/4 p-1">
-                        <Rating rate="3" />
-                      </div>
-                      <div class="w-1/4 p-1">
-                        開発可能
+                        知識レベル
                       </div>
                       <div class="w-1/4 p-1">
                         <Rating rate="2" />
@@ -102,68 +112,29 @@ const baseinfo = {
                     </div>
                     <div class="flex flex-wrap justify-items-center">
                       <div class="w-1/4 p-1">
-                        <Rating rate="1" />
+                        <Rating rate="3" />
                       </div>
                       <div class="w-1/4 p-1">
-                        知識レベル
+                        開発可能
+                      </div>
+                      <div class="w-1/4 p-1">
+                        <Rating rate="4" />
+                      </div>
+                      <div class="w-1/4 p-1">
+                        技術精通
+                      </div>
+                    </div>
+                    <div class="flex flex-wrap justify-items-center">
+                      <div class="w-1/4 p-1">
+                        <Rating rate="5" />
+                      </div>
+                      <div class="w-1/4 p-1">
+                        指導可能
                       </div>
                     </div>
                   </div>
 
                 </div>
-
-
-                <!--  -->
-                <!--  -->
-                <!-- Modal -->
-                <!--  -->
-                <!--  -->
-
-                <!-- Main modal -->
-                <!-- <div id="defaultModal" tabindex="-1" aria-hidden="true"
-                  class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-                  <div class="relative w-full h-full max-w-2xl md:h-auto">
-                    <div class="relative bg-white rounded-lg shadow-xl dark:bg-gray-700">
-                      <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                          経験技術
-                        </h3>
-                        <button type="button"
-                          class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                          data-modal-hide="defaultModal">
-                          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          <span class="sr-only">Close modal</span>
-                        </button>
-                      </div>
-                      <div class="p-6 space-y-6">
-                        <ExperienceRating :experienceRate="4" itemName="Java" />
-                        <ExperienceRating :experienceRate="4" itemName="C#" />
-                        <ExperienceRating :experienceRate="3" itemName="Python" />
-                        <ExperienceRating :experienceRate="2" itemName="C++" />
-                        <ExperienceRating :experienceRate="3" itemName="Django4.0" />
-                        <ExperienceRating :experienceRate="4" itemName="JQuery" />
-                      </div>
-                      <div
-                        class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="defaultModal" type="button"
-                          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">追加</button>
-                        <button data-modal-hide="defaultModal" type="button"
-                          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">閉じる</button>
-                      </div>
-                    </div>
-                  </div>
-                </div> -->
-                <!--  -->
-                <!--  -->
-                <!-- Modal -->
-                <!--  -->
-                <!--  -->
-
 
               </div>
 
@@ -178,10 +149,9 @@ const baseinfo = {
                         for="grid-password">
                         経験
                       </label>
-
-                      <ExperienceRating :experienceRate="3" itemName="Java" />
-                      <ExperienceRating :experienceRate="2" itemName="Docker" />
-                      <ExperienceRating :experienceRate="3" itemName="JavaScript" />
+                      <div v-for="item in experienceRate" :key="item.id">
+                        <ExperienceRating :experienceRate="item.level" :itemName="item.name" />
+                      </div>
                     </div>
 
                     <div class="mt-10">
