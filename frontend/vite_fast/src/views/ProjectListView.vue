13517c0759bf7ed1e0ca596adab177fdb1b0de69
myantyuWorld <nepinepi984@gmail.com>
2023-02-04 21:38:01 +0900

    Add 案件追加ボタン

--- frontend/vite_fast/src/views/ProjectListView.vue
+++ frontend/vite_fast/src/views/ProjectListView.vue
@@ -121,9 +121,12 @@ const data = {
             <div class="p-3 sm:p-3">
 
               <div class="space-y-0">
-                <h2 class="mb-0 text-2xl text-cyan-900 font-bold">
-                  業務履歴
-                </h2>
+                <div class="flex">
+                  <h2 class="mb-0 text-2xl text-cyan-900 font-bold">
+                    業務履歴
+                  </h2>
+                  <button type="button" class="ml-3 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">追加</button>
+                </div>
               </div>
               <div class="flex bg-white">
                 <div class="w-full px-4 shadow-none">
