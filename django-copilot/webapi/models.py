c135971a475c4bb0652252fe7daeb13bb132a10c
myantyuWorld <nepinepi984@gmail.com>
2023-02-05 19:35:15 +0900

    [django] Modify models.py

--- django-copilot/webapi/models.py
+++ django-copilot/webapi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Member(models.Model):
     name = models.CharField('氏名', max_length=255)
@@ -11,5 +12,30 @@ class Member(models.Model):
     class Meta:
         db_table = 'member'
 
+class Category(models.Model):
+    name = models.CharField('カテゴリ名', max_length=255)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return self.name
+
+
 class Technology(models.Model):
+    category = models.ForeignKey(Category, verbose_name="カテゴリID", on_delete=models.CASCADE)
     name = models.CharField('技術名', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'technology'
+
+# TODO : 以下、設計中
+# class ExperienceTechnology(models.Model):
+#     user_id = models.ForeignKey(get_user_model(), verbose_name="会員ID", on_delete=models.CASCADE)
+#     technology = models.ForeignKey(Technology, verbose_name="技術ID", on_delete=models.CASCADE)
+
+#     class Meta:
+#         db_table = 'experience_technology'
