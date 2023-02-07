96ba2cad23a86337fe4259c627c10b8697577977
myantyuWorld <nepinepi984@gmail.com>
2023-02-07 19:14:38 +0900

    Post時に、ForeignKey指定されたフィールドの登録できるようシリアライザ修正

--- django-copilot/webapi/serializer.py
+++ django-copilot/webapi/serializer.py
@@ -13,6 +13,18 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class TechnologySerializer(serializers.ModelSerializer):
      category = CategorySerializer(read_only=True)
+     category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True)
+
+     def create(self, validated_date):
+        validated_date['category'] = validated_date.get('category_id', None)
+
+        if validated_date['category'] is None:
+            raise serializers.ValidationError("category not found.") 
+
+        del validated_date['category_id']
+
+        return Technology.objects.create(**validated_date)
+     
      class Meta:
         model = Technology
         fields = '__all__'
